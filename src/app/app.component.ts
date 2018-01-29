a9277689137095755281db1d6bff2a0ca54e08a4
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2018-01-29 11:36:43 +0530

    Added more functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {TagModel} from "ngx-chips/core";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,37 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   items = ['Pizza', 'Pasta', 'Parmesan'];
+  fable = [];
+  validator: any;
+  public errorMessages = {
+    'startsWithAt@': 'Your items need to start with "@"',
+    'endsWith$': 'Your items need to end with "$"'
+  };
+  public onRemoving(tag: TagModel): Observable<TagModel> {
+    const confirm = window.confirm('Do you really want to remove this tag?');
+    return Observable
+      .of(tag)
+      .filter(() => confirm);
+  }
+  public onTextChange(event) {
+    console.log(event);
+    if (this.fable.length === 0) {
+      this.fable = ['Not found'];
+    }
+  }
+  public onItemAdded(event) {
+    console.log(event);
+  }
+  public startsWithAt(control: FormControl) {
+    if (control.value.charAt(0) !== '@') {
+      //this.fable = ['Not found'];
+      return {
+        'startsWithAt@': true
+      };
+    }
+
+    return null;
+  }
+  validators = [this.startsWithAt];
+
 }
